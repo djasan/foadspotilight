55dba9afa596841da1eb2c0350ec840830b34693
sanson djamila <sansondjamila@orange.fr>
2023-11-03 22:20:27 +0100

    c

--- assets/js/modules/catalogue.js
+++ assets/js/modules/catalogue.js
@@ -1,43 +1,43 @@
 const images = [
     {
-      url: '/assets/img/cover/img1.png',
+      url: 'img1.png',
       title: 'Les choses qui ne font que passer...',
       description: 'Le vent qui souffle... les oiseaux qui volent...la musique à la radio...',
 
     },
     {
-      url:'/assets/img/cover/img2.png',
+      url:'img2.png',
       title: 'Les choses qui ne font que passer...',
       description: 'Les saisons, les mois, les années...Les minutes, les heures, les jours...'
 
     },
     {
-      url: '/assets/img/cover/img3.png',
+      url: 'img3.png',
       title: 'Les choses qui ne font que passer...',
       description: 'Les trains...'
     },
     {
-        url: '/assets/img/cover/img4.png',
+        url: 'img4.png',
         title: 'Les choses qui ne font que passer...',
         description: 'La pub à la télé... Les affiches des films...'
       },
       {
-        url: 'assets/img/cover/img5.png',
+        url: 'img5.png',
         title: 'Les choses qui ne font que passer...',
         description: 'Un animal qui passe dans un pré...'
       },
       {
-        url: 'assets/img/cover/img6.png',
+        url: 'img6.png',
         title: 'Les choses qui ne font que passer...',
         description: 'Les nuits...'
       },
       {
-        url: 'assets/img/cover/img7.png',
+        url: 'img7.png',
         title: 'Les choses qui ne font que passer...',
         description: 'Une feuille qui tombe...'
       },
       {
-        url: 'assets/img/cover/img8.png',
+        url: 'img8.png',
         title: 'Les choses qui ne font que passer...',
         description: 'Les odeurs...'
       }
